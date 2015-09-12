86751f58f4bfd7fac4890c283c93fa659e9f958d
RobertBowie <robertb1903@gmail.com>
2015-09-12 14:20:43 -0700

    Completed _.filter()

--- src/underbar.js
+++ src/underbar.js
@@ -78,6 +78,11 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(val) {
+      if (test(val)) { result.push(val) };
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
